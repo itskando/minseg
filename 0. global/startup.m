a69b4fcbd1a54817a5573bef9f29b9d7e7140579
itskando <nate.kando@gmail.com>
2020-09-20 21:56:28 -0400

    Fixed startup.m bug. Documented arduino connection issues.

--- 0. global/startup.m
+++ 0. global/startup.m
@@ -1,11 +1,18 @@
 %% Libraries
 
-%  Set project library paths:
-libPath  = '../1. System Test Platform/1. Library/';
+switch 0
+case 1 % one time use: add startup to permanent path
+ path.start = '/Users/kando/Git/twoWheeledRobot/0. global';
+ addpath( path.start );
+ savepath
+end
 
-libPaths = genpath( libPath  ); % generates all library subpaths;
-           addpath( libPaths ); % adds      all library subpaths;
-         clearvars('libPath*'); % cleanup
+%  Set library paths for project:
+path.lib  = '../1. System Test Platform/1. Library/';
+
+path.libs = genpath  (  path.lib  ); % generates all library subpaths;
+            addpath  (  path.libs ); % adds      all library subpaths;
+            clearvars( 'path'     ); % cleanup
 
 %% Compiler
 
